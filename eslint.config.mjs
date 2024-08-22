5ab371af2e74c4502897394475b964c0782fab92
zzydannyer <1404631613@qq.com>
2024-08-22 17:16:32 +0800

    feat: 666

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,43 @@
-export default [
-  {
+import globals from 'globals'
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import importSort from 'eslint-plugin-simple-import-sort'
+
+export default tseslint.config({
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx,js}'],
+    ignores: ['apps/**/*/{tmp,.dumi}/**/*', '*.js', '**/*/build/**/*', '**/*/es/**/*', '**/*/dist/**/*'],
     rules: {
-      semi: "error",
-      "prefer-const": "error",
+        '@typescript-eslint/array-type': 'error',
+        '@typescript-eslint/no-for-in-array': 'error',
+        'no-unused-vars': 'error',
+        'no-undef': 'warn',
+        'no-console': 'error',
+        'simple-import-sort/imports': [
+            'error',
+            {
+                groups: [
+                    ['^\\w'],
+                    ['^@\\w'],
+                    ['^@/'],
+                    ['^\\u0000'],
+                    ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+                    ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$']
+                ]
+            }
+        ],
+        'simple-import-sort/exports': 'error'
     },
-  },
-];
+    languageOptions: {
+        parser: tseslint.parser,
+        globals: {
+            ...globals.browser,
+            ...globals.node
+        },
+        parserOptions: {
+            project: ['./tsconfig.eslint.json', '**/*/tsconfig.json'],
+            tsconfigRootDir: import.meta.dirname
+        }
+    },
+    plugins: { 'simple-import-sort': importSort }
+})
---
+++ stylelint.config.mjs
@@ -0,0 +1,9 @@
+export default {
+    extends: ['stylelint-config-standard'],
+    customSyntax: 'postcss-styled-syntax',
+    rules: {
+        'block-no-empty': true,
+        'comment-empty-line-before': null,
+        'selector-not-notation': null
+    }
+}
